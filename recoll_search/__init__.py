81c87a2ccc4f304b80bebcb2ce7175b529bfcb86
Nick_A_Kuznetsov <nick2kuz@gmail.com>
2026-09-20 16:40:11 +0300

    Работает пока без окна вывода вхождений

--- recoll_search/__init__.py
+++ recoll_search/__init__.py
@@ -1,13 +1,16 @@
+from urllib.parse import unquote, urlparse
+
 from gi.repository import Gtk
 
 from zim.plugins import PluginClass
 from zim.actions import action
 from zim.gui.mainwindow import MainWindowExtension
+from zim.newfs import LocalFile
 
 from recoll import recoll
 
-
 class RecollSearchPlugin(PluginClass):
+
     plugin_info = {
         'name': 'Recoll Search',
         'description': 'Search with Recoll',
@@ -18,33 +21,33 @@ class RecollSearchPlugin(PluginClass):
 class RecollSearchMainWindowExtension(MainWindowExtension):
 
     def __init__(self, plugin, window):
-        super().__init__(
-            plugin,
-            window
-        )
+        super().__init__(plugin, window)
 
         self.db = None
-        self.window = None
+        self.search_window = None
+        self.model = None
 
     @action('Recoll Search', menuhints='tools')
     def recoll_search(self):
 
-        if self.window is not None:
-            self.window.present()
+        if self.search_window is not None:
+            self.search_window.present()
             return
 
-        self.window = Gtk.Window(
+        self.search_window = Gtk.Window(
             title='Recoll Search'
         )
 
-        self.window.set_default_size(
-            800,
+        self.search_window.set_default_size(
+            900,
             600
         )
 
-        self.window.connect(
+        self.search_window.set_position(Gtk.WindowPosition.CENTER)
+
+        self.search_window.connect(
             'destroy',
-            self._on_destroy
+            self._on_window_destroy
         )
 
         box = Gtk.Box(
@@ -60,7 +63,6 @@ class RecollSearchMainWindowExtension(MainWindowExtension):
         )
 
         self.entry = Gtk.Entry()
-
         self.entry.set_hexpand(True)
 
         self.entry.connect(
@@ -98,26 +100,63 @@ class RecollSearchMainWindowExtension(MainWindowExtension):
             0
         )
 
-        self.textview = Gtk.TextView()
+        # Результаты
+        self.model = Gtk.ListStore(
+            str,  # title
+            str,  # filename
+            str,  # url
+            str,  # date
+            object,  # tags
+            object,  # snippets
+        )
 
-        self.textview.set_editable(False)
-        self.textview.set_cursor_visible(False)
-        self.textview.set_wrap_mode(
-            Gtk.WrapMode.WORD_CHAR
+        self.tree = Gtk.TreeView(
+            model=self.model
+        )
+
+        self.tree.set_headers_visible(False)
+
+        selection = self.tree.get_selection()
+        selection.set_mode(
+            Gtk.SelectionMode.SINGLE
+        )
+
+        selection.connect(
+            'changed',
+            self._selection_changed
+        )
+
+        renderer = Gtk.CellRendererText()
+
+        column = Gtk.TreeViewColumn(
+            'Result',
+            renderer,
+            text=0
+        )
+
+        column.set_expand(True)
+
+        self.tree.append_column(
+            column
+        )
+
+        self.tree.connect(
+            'row-activated',
+            self._row_activated
+        )
+
+        self.tree.connect(
+            'key-press-event',
+            self._key_press
         )
 
         scroll = Gtk.ScrolledWindow()
-
-        scroll.set_policy(
-            Gtk.PolicyType.AUTOMATIC,
-            Gtk.PolicyType.AUTOMATIC
-        )
 
         scroll.set_hexpand(True)
         scroll.set_vexpand(True)
 
         scroll.add(
-            self.textview
+            self.tree
         )
 
         box.pack_start(
@@ -127,7 +166,7 @@ class RecollSearchMainWindowExtension(MainWindowExtension):
             0
         )
 
-        self.window.add(
+        self.search_window.add(
             box
         )
 
@@ -139,7 +178,7 @@ class RecollSearchMainWindowExtension(MainWindowExtension):
                 + str(error)
             )
 
-        self.window.show_all()
+        self.search_window.show_all()
 
     def _search(self, widget):
 
@@ -151,39 +190,21 @@ class RecollSearchMainWindowExtension(MainWindowExtension):
         if self.db is None:
             return
 
-        buffer = self.textview.get_buffer()
-
-        buffer.set_text(
-            'Поиск...\n'
-        )
+        self.model.clear()
 
         try:
             query = self.db.query()
 
-            nres = query.execute(
+            query.execute(
                 text,
                 fetchtext=True
             )
 
-            output = []
+            for doc in query.fetchmany(20):
 
-            output.append(
-                'Запрос: %s' % text
-            )
-
-            output.append(
-                'Найдено: %d' % nres
-            )
-
-            output.append(
-                '=' * 70
-            )
-
-            for doc in query.fetchmany(10):
-
-                title = getattr(
+                url = getattr(
                     doc,
-                    'title',
+                    'url',
                     ''
                 )
 
@@ -193,80 +214,247 @@ class RecollSearchMainWindowExtension(MainWindowExtension):
                     ''
                 )
 
-                url = getattr(
-                    doc,
-                    'url',
-                    ''
+                title = self._get_title(
+                    doc
                 )
 
-                mtype = getattr(
-                    doc,
-                    'mtype',
-                    ''
+                date = self._get_date(
+                    doc
                 )
 
-                output.append('')
-                output.append(
-                    'TITLE: %s' % title
-                )
-                output.append(
-                    'FILE:  %s' % filename
-                )
-                output.append(
-                    'URL:   %s' % url
-                )
-                output.append(
-                    'TYPE:  %s' % mtype
+                tags = self._get_tags(
+                    doc
                 )
 
-                try:
-                    snippets = query.getsnippets(
-                        doc,
-                        maxoccs=3,
-                        ctxwords=8
-                    )
-                except Exception:
-                    snippets = []
-
-                if snippets:
-
-                    output.append('')
-                    output.append(
-                        'SNIPPETS:'
-                    )
-
-                    for page, term, snippet in snippets:
-
-                        output.append(
-                            '[%s] %s'
-                            % (
-                                term,
-                                snippet
-                            )
-                        )
-
-                output.append('')
-                output.append(
-                    '-' * 70
+                snippets = query.getsnippets(
+                    doc
                 )
 
-            buffer.set_text(
-                '\n'.join(output)
-            )
+                self.model.append(
+                    [
+                        title,
+                        filename,
+                        url,
+                        date,
+                        tags,
+                        snippets
+                    ]
+                )
 
             query.close()
 
         except Exception as error:
 
-            buffer.set_text(
+            self._show_error(
                 'Ошибка поиска:\n\n'
+                + str(error)
+            )
+
+    def _get_title(self, doc):
+
+        text = getattr(
+            doc,
+            'text',
+            ''
+        )
+
+        for line in text.splitlines():
+
+            line = line.strip()
+
+            if (
+                line.startswith('====== ')
+                and line.endswith(' ======')
+            ):
+                return line[7:-7].strip()
+
+        filename = getattr(
+            doc,
+            'filename',
+            ''
+        )
+
+        return filename
+
+    def _get_date(self, doc):
+
+        text = getattr(
+            doc,
+            'text',
+            ''
+        )
+
+        for line in text.splitlines():
+
+            line = line.strip()
+
+            if line.startswith('Creation-Date:'):
+
+                value = line[
+                    len('Creation-Date:'):
+                ].strip()
+
+                return value
+
+        return ''
+
+    def _get_tags(self, doc):
+
+        text = getattr(
+            doc,
+            'text',
+            ''
+        )
+
+        lines = text.splitlines()
+
+        title_index = None
+
+        for index, line in enumerate(lines):
+
+            line = line.strip()
+
+            if (
+                line.startswith('====== ')
+                and line.endswith(' ======')
+            ):
+                title_index = index
+                break
+
+        if title_index is None:
+            return []
+
+        date_index = title_index + 1
+
+        if date_index >= len(lines):
+            return []
+
+        date_line = lines[date_index].strip()
+
+        if not date_line:
+            return []
+
+        tags_index = date_index + 2
+
+        if tags_index >= len(lines):
+            return []
+
+        if lines[tags_index].strip() == '':
+            return []
+
+        tag_line = lines[tags_index].strip()
+
+        if not tag_line.startswith('@'):
+            return []
+
+        tags = [
+            tag
+            for tag in tag_line.split()
+            if tag.startswith('@')
+        ]
+
+        if not tags:
+            return []
+
+        return tags
+
+    def _selection_changed(self, selection):
+
+        model, iterator = selection.get_selected()
+
+        if iterator is None:
+            return
+
+        snippets = model[iterator][5]
+
+        print('\nSELECTED SNIPPETS:')
+
+        for snippet in snippets:
+            print(
+                repr(snippet)
+            )
+
+    def _row_activated(
+        self,
+        tree,
+        path,
+        column
+    ):
+
+        model = tree.get_model()
+
+        row = model[path]
+
+        url = row[2]
+
+        self._open_result(
+            url
+        )
+
+    def _key_press(self, tree, event):
+
+        if event.keyval == 65293:  # Enter
+
+            model, iterator = tree.get_selection().get_selected()
+
+            if iterator is not None:
+
+                url = model[iterator][2]
+
+                self._open_result(
+                    url
+                )
+
+                return True
+
+        return False
+
+    def _open_result(self, url):
+
+        if not url:
+            return
+
+        parsed = urlparse(url)
+
+        if parsed.scheme != 'file':
+            self._show_error(
+                'Это не локальный файл:\n\n'
+                + url
+            )
+            return
+
+        filename = unquote(
+            parsed.path
+        )
+
+        try:
+            file = LocalFile(filename)
+
+            path, file_type = self.window.notebook.layout.map_file(
+                file
+            )
+
+            page = self.window.notebook.get_page(
+                path
+            )
+
+            self.window.pageview.set_page(
+                page
+            )
+
+        except Exception as error:
+
+            self._show_error(
+                'Не удалось открыть страницу:\n\n'
+                + filename
+                + '\n\n'
                 + str(error)
             )
 
     def _show_error(self, message):
 
         dialog = Gtk.MessageDialog(
-            transient_for=self.window,
+            transient_for=self.search_window,
             modal=True,
             message_type=Gtk.MessageType.ERROR,
             buttons=Gtk.ButtonsType.OK,
@@ -280,9 +468,9 @@ class RecollSearchMainWindowExtension(MainWindowExtension):
         dialog.run()
         dialog.destroy()
 
-    def _on_destroy(self, window):
+    def _on_window_destroy(self, window):
 
-        self.window = None
+        self.search_window = None
 
     def do_deactivate(self):
 
@@ -295,10 +483,9 @@ class RecollSearchMainWindowExtension(MainWindowExtension):
 
             self.db = None
 
-        if self.window is not None:
+        if self.search_window is not None:
 
-            self.window.destroy()
-
-            self.window = None
+            self.search_window.destroy()
+            self.search_window = None
 
         super().do_deactivate()
